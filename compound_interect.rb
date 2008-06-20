b316ba37617b44de1325cfa9ab6e6acc63ac63c1
idesaku <idesaku@gmail.com>
2008-06-20 17:07:12 +0900

    scanよりsplitのほうが短くかける。

--- compound_interect.rb
+++ compound_interect.rb
@@ -1,7 +1,6 @@
-STDIN.read.scan(/(\d+):(\d+):(\d+)/) { |a,b,c|
-  x = b.to_i
-  puts x
-  (1...a.to_i).each{
-    puts x = (x * (1.0+c.to_f/100)).to_i
-  }
+a,b,c=STDIN.read.split(/:/)
+x = b.to_i
+puts x
+(1...a.to_i).each{
+  puts x = (x * (1.0+c.to_f/100)).to_i
 }
